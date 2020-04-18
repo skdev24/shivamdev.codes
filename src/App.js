fa610b273d4284f51916298147c0f1c0295ed97b
Shivam dev <skdev24@gmail.com>
2020-04-18 20:56:38 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const screenWidth = window.innerWidth;
+const mobileWidth =
+  // eslint-disable-next-line no-nested-ternary
+  screenWidth <= 500
+    ? 400
+    : screenWidth <= 1000
+    ? 400
+    : 400;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '0px',
+        height: '100%',
+        overflow: 'hidden',
+        flexDirection: 'column',
+        paddingTop: '30px',
+      }}
+    >
+      <div
+        style={{
+          position: 'relative',
+          width: mobileWidth,
+          height: 700,
+        }}
+      >
+        <img
+          style={{
+            position: 'absolute',
+            display: 'flex',
+            width: mobileWidth,
+            alignSelf: 'center',
+            height: 'auto'
+          }}
+          resize="contain"
+          className=""
+          src={require('./assets/shivamdev.png')}
+          alt=""
+        />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            position: 'absolute',
+            bottom: 80,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%'
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {[
+            'https://twitter.com/Kdev24S',
+            'https://instagram.com/d_sk04/',
+            'https://linkedin.com/in/skdev24/',
+            'https://github.com/skdev24'
+          ].map((item, index) => (
+            // eslint-disable-next-line max-len
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
+            <a
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              style={{
+                height: mobileWidth * 0.15,
+                width: mobileWidth * 0.16,
+              }}
+              href={item}
+              // eslint-disable-next-line react/jsx-no-target-blank
+              target="_blank"
+            />
+          ))}
+        </div>
+      </div>
+      <div
+        style={{
+          height: '100px',
+          width: '200px'
+        }}
+      />
     </div>
   );
 }
